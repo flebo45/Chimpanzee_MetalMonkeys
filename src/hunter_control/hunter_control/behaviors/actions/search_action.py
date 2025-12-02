3361b5c3891ced20035ec0e12a33c8efe3d4c844
mattpaol441 <paolinomatteo@gmail.com>
2025-12-02 17:28:56 +0100

    Refactor search action with speed adjustment and logging
    Updated the search action to adjust rotation speed and added logging.

--- src/hunter_control/hunter_control/behaviors/actions/search_action.py
+++ src/hunter_control/hunter_control/behaviors/actions/search_action.py
@@ -2,20 +2,23 @@ import py_trees
 from geometry_msgs.msg import Twist
 
 class ActionSearch(py_trees.behaviour.Behaviour):
-    """
-    Ruota su se stesso per cercare il target
-    """
+    """ Ruota su se stesso per cercare il target """
     def __init__(self, name="Search (Spin)"):
         super().__init__(name=name)
         self.publisher = None
+        self.node = None
 
     def setup(self, **kwargs):
         self.node = kwargs.get('node')
         self.publisher = self.node.create_publisher(Twist, '/cmd_vel', 10)
 
     def update(self):
-        msg = Twist()
-        msg.angular.z = 0.5 # Rotazione costante
-        self.publisher.publish(msg)
-        self.feedback_message = "Searching..."
+        cmd = Twist()
+        cmd.angular.z = 0.4
+        self.publisher.publish(cmd)
+        
+        # LOG
+        if self.node:
+            self.node.get_logger().info('SEARCHING...', throttle_duration_sec=2.0)
+            
         return py_trees.common.Status.RUNNING
